ede2574a8a9ee4cef2fdb28454dabc9f15144370
serj <sergiustn1@gmail.com>
2022-01-23 12:55:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 import './App.css';
 
 function App() {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '4fcb3720e205b07c0f6b3a4c98949331',
+    lat: '46.5326',
+    lon: '24.5671',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        locationLabel="Targu-Mures"
+        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+        showForecast
+      />
     </div>
   );
 }
